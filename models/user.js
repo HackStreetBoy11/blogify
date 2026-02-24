0f1ae8b706a3e28ec8f09d7b5206d037f8a51025
Varun <varun@gmail.com>
2026-02-24 23:26:37 +0530

    hash password

--- models/user.js
+++ models/user.js
@@ -41,6 +41,20 @@ userSchema.pre('save', async function () {
 
 })
 
+userSchema.static('matchPasword', async function (email, password) {
+    const user = await this.findOne({ email });
+    if (!user) throw new Error('User not found!');
+
+    const salt = user.salt;
+
+    const hashedPassword = user.password;
+
+    const userProvidedHash = createHmac("sha256", salt).update(password).digest("hex");
+    if (hashedPassword != userProvidedHash) throw new Error('Incorrect password');
+    return user;
+})
+
+
 const User = model('user', userSchema)
 
 module.exports = User;
